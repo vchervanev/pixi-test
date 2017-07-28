3a9b2edc4fdd4d303375bbdefef3d5db5c56591f
vladmir.chervanev <vladimir.chervanev@envato.com>
2017-07-28 23:11:05 +1000

    fun: circles

--- app/js/index.js
+++ app/js/index.js
@@ -1,39 +1,37 @@
 import _ from './module1'
 require('pixi.js')
 
-var app = new PIXI.Application(800, 600, { antialias: true });
+var app = new PIXI.Application(300, 300, 
+    { 
+        antialias: true,
+        backgroundColor: 0xAABBCC,
+        resolution: 2
+    });
 document.body.appendChild(app.view);
 
 var graphics = new PIXI.Graphics();
 
-// set a fill and line style
-graphics.beginFill(0xFF3300);
-graphics.lineStyle(4, 0xffd900, 1);
-
-// draw a shape
-graphics.moveTo(50,50);
-graphics.lineTo(250, 50);
-graphics.lineTo(100, 100);
-graphics.lineTo(50, 50);
-graphics.endFill();
-
-// set a fill and a line style again and draw a rectangle
-graphics.lineStyle(2, 0x0000FF, 1);
-graphics.beginFill(0xFF700B, 1);
-graphics.drawRect(50, 250, 120, 120);
-
-// draw a rounded rectangle
-graphics.lineStyle(2, 0xFF00FF, 1);
-graphics.beginFill(0xFF00BB, 0.25);
-graphics.drawRoundedRect(150, 450, 300, 100, 15);
-graphics.endFill();
+app.stage.interactive = true
+app.stage.click = function(event) { console.log(event) }
 
 // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
 graphics.lineStyle(0);
-graphics.beginFill(0xFFFF0B, 0.5);
-graphics.drawCircle(470, 90,60);
+graphics.beginFill(0xFFFF00, 0.9);
+graphics.drawCircle(30, 30, 30);
 graphics.endFill();
 
 app.stage.addChild(graphics);
 
-console.log('index.js!');
+var roundMotion = (graphics, steps, point, radius) => {
+    var sin = Math.sin
+    var cos = Math.cos
+    var r = radius
+    var k = 0
+    return () => {
+        k = (k + 1 / steps) % 1
+        var a = 2*Math.PI*k
+        graphics.position.set(point.x + r*cos(a), point.y + r*sin(a))
+    }
+}
+
+app.ticker.add(roundMotion(graphics, 80, {x: 100, y: 100}, 50))
