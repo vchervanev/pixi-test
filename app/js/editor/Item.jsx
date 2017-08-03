87e3e8e7b0fea8725297a6fa4312f2f4c6088a6c
vladmir.chervanev <vladimir.chervanev@envato.com>
2017-08-03 21:58:38 +1000

    fixed webpack dev server

--- app/js/editor/Item.jsx
+++ app/js/editor/Item.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Item = (props) => (<div>R:{item.radius}, S: {item.speed} </div>)
+const Item = (props) => (<div>R:{props.data.radius}, S: {props.data.speed} </div>)
 // item.children && item.children.map((item, j) => (
 // <div key={j)}>
 //     R:{ item.radius }, S: { item.speed } 
